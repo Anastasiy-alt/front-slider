04e41387a9b3487b08dfadc01d8ad9cca73f391c
Анастасия <n060600@yandex.ru>
2023-10-19 01:45:28 +0500

    feat: верстка слайдера после одинарного клика

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,11 @@ export default {
         './assets/dots=activeSecondSlide.svg',
         './assets/dots=activeThirdSlide.svg'
       ],
-      currentIndex: 0
-    }
+      currentIndex: 0,
+      activeIndex: 0,
+      showThumbnails: true,
+      imageFullScreen: false
+    };
   },
   computed: {
     currentSlide() {
@@ -27,6 +30,7 @@ export default {
       } else {
         this.currentIndex--;
       }
+      this.activeIndex = this.currentIndex;
     },
     nextSlide() {
       if (this.currentIndex === this.slides.length - 1) {
@@ -34,37 +38,57 @@ export default {
       } else {
         this.currentIndex++;
       }
+      this.activeIndex = this.currentIndex;
+    },
+    toggleThumbnails() {
+      // this.showThumbnails = !this.showThumbnails;
+    },
+    openFullScreenImage() {
+        this.imageFullScreen = true;
     }
   },
   mounted() {
-    setInterval(() => {
-      this.nextSlide();
-    }, 30000);
+    setInterval(() => { this.nextSlide(); }, 30000);
+  },
+  setCurrentSlide(index) {
+    this.currentIndex = index;
+    this.activeIndex = index;
   }
 }
-
 </script>
 
 <template>
   <div class="slider">
-    <div class="slider__img-box">
+    <!-- <div class="slider__img-box" v-if="!showThumbnails" >
       <button @click="prevSlide" class="slider__button slider__button_type_prev"></button>
-      <img :src="currentSlide" class="slider__img" :key="currentIndex">
+      <img :src="currentSlide" class="slider__img" :key="currentIndex" @click="toggleThumbnails"
+        @dblclick="addImageBorder">
       <button @click="nextSlide" class="slider__button slider__button_type_next"></button>
     </div>
-    <div class="slider__dots">
-      <span
-        v-for="(slide, index) in slides"
-        :key="index"
-        class="slider__dot"
-        :class="{ 'slider__dot_active': index === currentIndex }"
-      ></span>
+    <div class="slider__dots" v-if="!showThumbnails" >
+      <span v-for="(slide, index) in slides" :key="index" class="slider__dot"
+        :class="{ 'slider__dot_active': index === currentIndex }"></span>
+    </div> -->
+
+    <div class="slider__one-click">
+      <button @click="prevSlide" class="slider__button slider__button_type_prev"></button>
+      <img :src="currentSlide" class="slider__img slider__img_size_medium" :key="currentIndex"
+      @dblclick="openFullScreenImage" :class="{ 'slider__img_size_full': imageFullScreen }">
+      <button @click="nextSlide" class="slider__button slider__button_type_next"></button>
+      <div v-if="showThumbnails" class="slider__thumbnails">
+        <img v-for="(slide, index) in slides" :key="index" :src="slide"
+          :class="{ 'slider__thumbnail_active': index === activeIndex }" @click="setCurrentSlide(index)"
+          class="slider__thumbnail-img" />
+      </div>
     </div>
+
 
   </div>
 </template>
 
 <style scoped>
-
+/* *,*::before,*::after {outline: 1px solid red} */
 </style>
 
+
+<!-- :class="{ 'slider__img-bordered': imageBorder }" -->
